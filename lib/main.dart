9b306d9fdc5971d0972d19854412e39848445cb5
Lucas Vermeulen <lucasvermeulen@gmail.com>
2023-09-29 15:22:38 +0200

    remove debug show

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '500 miles app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
